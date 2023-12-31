e155754313e6cee09eeb673833dff3d3ef6f47f0
Harri Nieminen <nieminen.harri@gmail.com>
2023-12-31 14:38:47 +0200

    final

--- electricsite/settings.py
+++ electricsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #'automated_logging'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    #'automated_logging.middleware.AutomatedLoggingMiddleware'
 ]
 
 ROOT_URLCONF = "electricsite.urls"
