2161434e44f4a67296f004d7cbeeb2e599bfb22d
Harri Nieminen <nieminen.harri@gmail.com>
2024-01-17 12:51:16 +0200

    admin update

--- electricsite/settings.py
+++ electricsite/settings.py
@@ -89,13 +89,12 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-        #"OPTIONS": {
-        #    "min_length": 9,
-        #},
+    #{"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    #    "OPTIONS": {
+    #        "min_length": 12,
+    #    },
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
